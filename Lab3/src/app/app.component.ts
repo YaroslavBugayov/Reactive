a63dfded704f228fb96a7d61cf3b2e61a7d8a37a
YaroslavBugayov <bugayov576@gmail.com>
2023-12-06 00:33:48 +0200

    lab3

---
+++ Lab3/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import {TodoList} from "./todoList";
+import {TodoItem} from "./todoItem";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  private list = new TodoList("Yaroslav", [
+    new TodoItem("Do the third laboratory work"),
+    new TodoItem("Get some sleep"),
+    new TodoItem("Submit laboratory work")
+  ])
+
+  get username(): string {
+    return this.list.user
+  }
+
+  get itemCount(): number {
+    return this.list.items.filter(item => !item.complete).length
+  }
+
+  get items(): readonly TodoItem[] {
+    return this.list.items.filter(item => this.showComplete || !item.complete)
+  }
+
+  addItem(newItem: string) {
+    if (newItem != "") {
+      this.list.addItem(newItem)
+    }
+  }
+
+  showComplete: boolean = false
+}
